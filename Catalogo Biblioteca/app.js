d67ee7840a3f9b3a028cc321ed08fd04da60a084
Isaac Amorim <163526904+isaaczinrs2@users.noreply.github.com>
2025-04-29 11:33:55 -0300

    Delete Catalogo Biblioteca directory

--- Catalogo Biblioteca/app.js
+++
@@ -1,6 +0,0 @@
-var app = require('./config/express.js')();
-var rotasProdutos = require('./app/routes/produtos.js')(app);
-
-app.listen(3000, function(){
-    console.log('Servidor Rodando!');
-});
